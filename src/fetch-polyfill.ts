54f99c224e43d9ce2f2d0eff2782bfd05d1fe539
brittonhayes <46035482+brittonhayes@users.noreply.github.com>
2022-12-07 02:36:31 +0000

    Deploying to gh-pages from @ brittonhayes/homeassistant-ts@6bfd4597e939990d47c8618ea4b4a964e76f911b 🚀

--- src/fetch-polyfill.ts
+++
@@ -1,9 +0,0 @@
-// eslint-disable @typescript-eslint/no-explicit-any
-import fetch, { Headers, Request, Response } from "node-fetch";
-
-if (!globalThis.fetch) {
-  globalThis.fetch = fetch as any;
-  globalThis.Headers = Headers as any;
-  globalThis.Request = Request as any;
-  globalThis.Response = Response as any;
-}
--- src/logger.ts
+++
@@ -1,8 +0,0 @@
-import { Middleware } from "openapi-typescript-fetch";
-
-export const logger: Middleware = async (url, init, next) => {
-  console.log(`fetching ${url}`);
-  const response = await next(url, init);
-  console.log(`fetched ${url}`);
-  return response;
-};
